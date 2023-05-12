c19ef76c93036585522353c2b8611616f2a9edad
selene-cho <selene.cho29@gmail.com>
2023-05-13 03:59:11 +0900

    6.4 cleanup

--- movie-web-service/src/App.js
+++ movie-web-service/src/App.js
@@ -1,45 +1,21 @@
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState('');
-
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
+function Hello() {
+  // useEffect 안에 return 넣어주면 cleanUp function 자주 쓰진 않지만 알아두면 좋음
   useEffect(() => {
-    console.log('I run only once.');
+    console.log('hi :)');
+    return () => console.log('bye :(');
   }, []);
+  return <h1>Hello</h1>;
+}
 
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("I run when 'keyword & counter' changes.");
-  }, [keyword, counter]);
-
-  // useEffect(() => {
-  //   // keyword가 비어있지 않고 길이가 5자 이상일때, keyword state 변할때마다 아래 console.log 실행
-  //   if (keyword !== '' && keyword.length > 5) {
-  //     console.log('SEARCH FOR', keyword);
-  //   }
-  // }, [keyword]);
-
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        type="text"
-        placeholder="Search here..."
-        onChange={onChange}
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
