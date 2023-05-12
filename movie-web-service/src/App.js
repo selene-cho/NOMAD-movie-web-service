b86b094bcfff55f18dd36a29f218105152bb8780
selene-cho <selene.cho29@gmail.com>
2023-05-13 03:20:29 +0900

    6.1 useEffect

--- movie-web-service/src/App.js
+++ movie-web-service/src/App.js
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log('call an api');
+  console.log('I run all the time');
+  useEffect(() => {
+    console.log('CALL THE API....');
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
