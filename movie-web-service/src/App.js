3adbd3da3e88fc52be748a175138a974f0310fdc
selene-cho <selene.cho29@gmail.com>
2023-05-14 01:58:02 +0900

    7.0 Practice Movie App / To Do List part 1.

--- movie-web-service/src/App.js
+++ movie-web-service/src/App.js
@@ -1,21 +1,29 @@
-import { useState, useEffect } from 'react';
-
-function Hello() {
-  // useEffect 안에 return 넣어주면 cleanUp function 자주 쓰진 않지만 알아두면 좋음
-  useEffect(() => {
-    console.log('hi :)');
-    return () => console.log('bye :(');
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from 'react';
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return; // toDo 비어 있으면 이 함수 작동 하지 않도록
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          type="text"
+          placeholder="Write your to do... "
+          onChange={onChange}
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
