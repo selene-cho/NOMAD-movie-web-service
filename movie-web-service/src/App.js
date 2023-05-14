365f8d1b3e480f28eff8c6ef19a9bff12b610bc9
selene-cho <selene.cho29@gmail.com>
2023-05-15 00:37:08 +0900

    7.2 Coin Tracker / code challenge

--- movie-web-service/src/App.js
+++ movie-web-service/src/App.js
@@ -1,36 +1,67 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return; // toDo 비어 있으면 이 함수 작동 하지 않도록
-    }
-    // create new array
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
+  const [loading, setLoading] = useState(true);
+  const [isChanged, setIsChanged] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [coin, setCoin] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [changeToCoin, setChangeToCoin] = useState(0);
+  const onChange = (e) => {
+    setCoin(e.target.value);
   };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('coin', coin);
+    console.log('amount', amount);
+    setChangeToCoin(amount / coin);
+  };
+  const handleInput = (e) => {
+    setAmount(e.target.value);
+  };
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+        setIsChanged(true);
+      });
+  }, []);
+  useEffect(() => {
+    setIsChanged(false);
+  }, [changeToCoin]);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          type="text"
-          placeholder="Write your to do... "
-          onChange={onChange}
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <>
+          <select onChange={onChange}>
+            <option>Select Coin!</option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <div>
+            <form onSubmit={onSubmit}>
+              <label htmlFor="money">Enter Your Money : $</label>
+              <input
+                id="money"
+                value={amount}
+                type="number"
+                placeholder="USD"
+                onChange={handleInput}
+              />
+              <button>Change To Coin</button>
+            </form>
+          </div>
+          {isChanged ? null : <h3>You can get {changeToCoin} coin</h3>}
+        </>
+      )}
     </div>
   );
 }
