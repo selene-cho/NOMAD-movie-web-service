7a45d37119a295702c933a8aa1c85eba91da3384
selene-cho <selene.cho29@gmail.com>
2023-05-15 02:05:37 +0900

    7.4 Movie App Part2

---
+++ movie-web-service/src/components/Movie.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+export default function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  // genres는 string을 가진 array여야 함
+};
