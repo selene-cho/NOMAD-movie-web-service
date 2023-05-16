4717fb530cd58456a1f50a3a56a6453e49739a4e
selene-cho <selene.cho29@gmail.com>
2023-05-17 02:36:50 +0900

    7.9 Styles

--- movie-web-service/src/components/Movie.js
+++ movie-web-service/src/components/Movie.js
@@ -1,19 +1,31 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from './Movie.module.css';
 
-export default function Movie({ id, coverImg, title, summary, genres }) {
+export default function Movie({ id, year, coverImg, title, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.container}>
+      <img src={coverImg} alt={title} className={styles.img} />
+      <div className={styles.content}>
+        <h2 className={styles.title}>
+          <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>🎬 {title}</Link>
+        </h2>
+        <h3 className={styles.year}>{year}</h3>
+        {/* slice(0,245) 0부터 245까지 잘라냄 */}
+        <p className={styles.summary}>
+          {/* {summary.length > 245 ? `${summary.slice(0, 245)}...` : summary} */}
+          {summary.length === 0
+            ? '-'
+            : summary.length > 245
+            ? `${summary.slice(0, 245)}...`
+            : summary}
+        </p>
+        <ul className={styles.genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
